585622c4b94e45aac2d5ab28201fffbfa40bec75
Rafael Patiño <rpatinyo@insca.com>
2023-02-15 13:22:34 +0100

    add parts list

--- xml2label/printLabelFromXml.py
+++ xml2label/printLabelFromXml.py
@@ -12,11 +12,15 @@ from jinja2 import Environment, FileSystemLoader
 
 @xw.sub
 def main():
+    pp = pprint.PrettyPrinter(sort_dicts=False, indent=2)
     
     UniquePattern = {}
     UniquePatternList = []
     UniqueUsedBoardData = {}
     ListUniqueUsedBoardData = []
+
+    UniqueUsedPartData = {}
+    ListUniqueUsedPartData = []
 
     tree = ET.parse('C:/vs-projects/apps-insca/xml2label/04159899.xml')
     root = tree.getroot()
@@ -43,6 +47,7 @@ def main():
     for board in root.findall('Board'):
         for rec in UniquePatternList:
             if rec['id'] == board.get('id'):
+                # Añadir a diccionario
                 UniqueUsedBoardData = ({
                     'id': board.get('id'),
                     'L': str(board.get('L')).replace('.', ','),
@@ -53,18 +58,47 @@ def main():
                     })
                 ListUniqueUsedBoardData.append(UniqueUsedBoardData)
     # print('Tableros enteros: ', len(ListUniqueUsedBoardData))
-    # pp = pprint.PrettyPrinter(sort_dicts=False, indent=2)
     # pp.pprint(ListUniqueUsedBoardData)
 
-    # Cantidad de piezas cortadas por patrón
+    # Cantidad de piezas cortadas
     for piece in root.iter('Piece'):
-        for sid in piece.iter('Sid'):
-            # print(piece.attrib, sid.attrib)
-            for pattern in child.findall('Pattern'):
-                if pattern.get('id') == sid.get('id'):
-                    print('De la pieza', piece.get('N'), 'de', piece.get('L'), 'X',
-                    piece.get('W'),'hay que fabricar', piece.get('Q'), 'con el patrón',
-                    pattern.get('id'), 'y el tablero', pattern.get('BrdNo'))
+        # Añadir a diccionario
+        UniqueUsedPartData = ({
+        'id': piece.get('N'),
+        'L': str(piece.get('L')).replace('.', ','),
+        'W': str(piece.get('W')).replace('.', ','),
+        'Qty': piece.get('Q') 
+        })
+        for part in root.findall('Part'):
+            if piece.get('N') == part.get('Code'):
+                UniqueUsedPartData.update({
+                    'Op': part.get('Desc1'),
+                    'BrdCode': part.get('Desc2'),
+                    })
+        ListUniqueUsedPartData.append(UniqueUsedPartData)
+    pp.pprint(ListUniqueUsedPartData)
+
+
+    # Cantidad de piezas cortadas por patrón
+    # for piece in root.iter('Piece'):
+    #     for sid in piece.iter('Sid'):
+    #         # print(piece.attrib, sid.attrib)
+    #         for pattern in child.findall('Pattern'):
+    #             if pattern.get('id') == sid.get('id'):
+    #                 print('De la pieza', piece.get('N'), 'de', piece.get('L'), 'X',
+    #                 piece.get('W'),'hay que fabricar', piece.get('Q'), 'con el patrón',
+    #                 pattern.get('id'), 'y el tablero', pattern.get('BrdNo'))
+    #                 # Añadir a diccionario
+    #                 UniqueUsedPartData = ({
+    #                 'id': piece.get('N'),
+    #                 'L': str(piece.get('L')).replace('.', ','),
+    #                 'W': str(piece.get('W')).replace('.', ','),
+    #                 'Qty': piece.get('Q') 
+    #                 })
+    #             ListUniqueUsedPartData.append(UniqueUsedPartData)
+    # print(ListUniqueUsedPartData)
+    # for rec in ListUniqueUsedPartData:
+    #     print(rec)
 
     # Definición de plantilla y variables
     environment = Environment(loader=FileSystemLoader('C:/vs-projects/apps-insca/xml2label/templates/'))
@@ -72,7 +106,9 @@ def main():
     template_vars = {"title" : root.get('name') ,
                      "date": date,
                      "code": code,  
-                     "boards": ListUniqueUsedBoardData}
+                     "boards": ListUniqueUsedBoardData,
+                     "parts": ListUniqueUsedPartData,
+                     }
     html_out = template.render(template_vars)
     HTML(string=html_out).write_pdf('C:/vs-projects/apps-insca/xml2label//templates/report.pdf')
     os.startfile('C:/vs-projects/apps-insca/xml2label/templates/report.pdf')
