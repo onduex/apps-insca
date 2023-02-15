1eea0fe1b2783ecc71cdcba0efc99cc98d4deba6
Rafael Patiño <rpatinyo@insca.com>
2023-02-15 13:45:51 +0100

    update

--- xml2label/printLabelFromXml.py
+++ xml2label/printLabelFromXml.py
@@ -22,7 +22,7 @@ def main():
     UniqueUsedPartData = {}
     ListUniqueUsedPartData = []
 
-    tree = ET.parse('C:/vs-projects/apps-insca/xml2label/04159899.xml')
+    tree = ET.parse('C:/vs-projects/apps-insca/xml2label/03964113.xml')
     root = tree.getroot()
     for child in root:
         # print(child.tag)
