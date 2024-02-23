108c0252de5696fc40403cc4973b5ec5ed3dc50f
Tomás Pascual <tomas.pascual@onduex.com>
2024-02-23 13:18:32 +0100

    [IMP] xml2excel update

--- xml2excel/extractFromXmlToExcel.py
+++ xml2excel/extractFromXmlToExcel.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import os
 import pprint
 import xml.etree.ElementTree as eT
 import xlwings as xw
@@ -8,48 +9,84 @@ import xlwings as xw
 
 @xw.sub
 def main():
-    list_unique_used_part_data = []
-
     # Usar pp.pprint()
     pp = pprint.PrettyPrinter(sort_dicts=False, indent=0)
 
     wb = xw.Book.caller()
     sheet = wb.sheets["DATA"]
-    xml_name = str("00530293")
-    path = str('O:/XmlToRead/' + xml_name + '.xml')
-    tree = eT.parse(path)
-    root = tree.getroot()
-    job_time = root.find('Param').find('AddParam').get('JobTime')
-    sum_sup = 0.0
-    seg_m2 = 0.0
 
-    # Cantidad de piezas cortadas
-    for part in root.iter('Part'):
-        # Añadir a diccionario
-        sup = (float(part.get('L')) / 1000 * float(part.get('W')) / 1000) * float(part.get('qMin'))
-        unique_used_part_data = ({
-            'id': part.get('id'),
-            'L': float(part.get('L')),
-            'W': float(part.get('W')),
-            'qMin': part.get('qMin'),
-            'Code': part.get('Code'),
-            'MatNo': part.get('MatNo'),
-            'Desc1': part.get('Desc1'),
-            'Desc2': part.get('Desc2'),
-            'Material': part.get('Material'),
-            'Sup': float(sup),
-            'JobTime': job_time,
-        })
-        list_unique_used_part_data.append(unique_used_part_data)
-    pp.pprint(len(list_unique_used_part_data))
+    directory = str('O:/XmlToRead')
+    file_names = os.listdir(directory)
+    list_unique_used_part_data = []
 
-    for i in range(len(list_unique_used_part_data)):
-        sum_sup += list_unique_used_part_data[i]['Sup']
-        seg_m2 = float(list_unique_used_part_data[i]['JobTime']) / float(sum_sup)
+    for rec in file_names:
+        sum_sup = 0
+        if rec.endswith('.xml'):
+            path = str('O:/XmlToRead/' + rec)
+            print(path)
+            tree = eT.parse(path)
+            root = tree.getroot()
+            job_time = root.find('Param').find('AddParam').get('JobTime')
+            seg_m2 = 0
 
-    # Escribir en excel
-    for i in range(len(list_unique_used_part_data)):
-        if i == 0:
+            # Cantidad de piezas cortadas
+            for part in root.iter('Part'):
+                # Añadir a diccionario
+                sup = (float(part.get('L')) / 1000 * float(part.get('W')) / 1000) * float(part.get('qMin'))
+                unique_used_part_data = ({
+                    'id': part.get('id'),
+                    'L': float(part.get('L')),
+                    'W': float(part.get('W')),
+                    'qMin': part.get('qMin'),
+                    'Code': part.get('Code'),
+                    'MatNo': part.get('MatNo'),
+                    'Desc1': part.get('Desc1'),
+                    'Desc2': part.get('Desc2'),
+                    'Material': part.get('Material'),
+                    'Sup': float(sup),
+                    'JobTime': '',
+                })
+                list_unique_used_part_data.append(unique_used_part_data)
+
+            for j in range(wb.sheets[0].range('J' + str(wb.sheets[0].cells.last_cell.row)).end('up').row - 1,
+                           len(list_unique_used_part_data)):
+                if list_unique_used_part_data[j]['Sup']:
+                    sum_sup += float(list_unique_used_part_data[j]['Sup'])
+
+            list_unique_used_part_data.append({
+                    'id': '--',
+                    'L': '--',
+                    'W': '--',
+                    'qMin': '--',
+                    'Code': '--',
+                    'MatNo': '--',
+                    'Desc1': '--',
+                    'Desc2': '--',
+                    'Material': '--',
+                    'Sup': sum_sup,
+                    'JobTime': job_time,
+                })
+
+            list_unique_used_part_data.append({
+                'id': '',
+                'L': '',
+                'W': '',
+                'qMin': '',
+                'Code': '',
+                'MatNo': '',
+                'Desc1': '',
+                'Desc2': '',
+                'Material': '',
+                'Sup': '',
+                'JobTime': '',
+            })
+
+            # pp.pprint(len(list_unique_used_part_data))
+
+        # Escribir en excel
+        for i in range(wb.sheets[0].range('A' + str(wb.sheets[0].cells.last_cell.row)).end('up').row - 1,
+                       len(list_unique_used_part_data)):
+            print(i)
             sheet["A" + str(i + 2)].value = list_unique_used_part_data[i]['id']
             sheet["B" + str(i + 2)].value = list_unique_used_part_data[i]['L']
             sheet["C" + str(i + 2)].value = list_unique_used_part_data[i]['W']
@@ -61,18 +98,6 @@ def main():
             sheet["I" + str(i + 2)].value = list_unique_used_part_data[i]['Material']
             sheet["J" + str(i + 2)].value = list_unique_used_part_data[i]['Sup']
             sheet["K" + str(i + 2)].value = list_unique_used_part_data[i]['JobTime']
-            sheet["L" + str(i + 2)].value = seg_m2
-        else:
-            sheet["A" + str(i + 2)].value = list_unique_used_part_data[i]['id']
-            sheet["B" + str(i + 2)].value = list_unique_used_part_data[i]['L']
-            sheet["C" + str(i + 2)].value = list_unique_used_part_data[i]['W']
-            sheet["D" + str(i + 2)].value = list_unique_used_part_data[i]['qMin']
-            sheet["E" + str(i + 2)].value = list_unique_used_part_data[i]['Code']
-            sheet["F" + str(i + 2)].value = list_unique_used_part_data[i]['MatNo']
-            sheet["G" + str(i + 2)].value = list_unique_used_part_data[i]['Desc1']
-            sheet["H" + str(i + 2)].value = list_unique_used_part_data[i]['Desc2']
-            sheet["I" + str(i + 2)].value = list_unique_used_part_data[i]['Material']
-            sheet["J" + str(i + 2)].value = list_unique_used_part_data[i]['Sup']
 
 
 if __name__ == "__main__":
